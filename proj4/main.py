001dda7942147830cf0d17f8ed785532649a6d8a
liscak <64167691+liscak@users.noreply.github.com>
2020-05-15 15:47:49 +0200

    -

--- proj4/main.py
+++ proj4/main.py
@@ -4,9 +4,15 @@ from time import sleep
 import torch
 
 dynet = DynamicNet()
+optimizer = torch.optim.SGD(params=dynet.parameters(), lr=0.01)
+criterion = torch.nn.MSELoss()
 
 while True:
     x_train = torch.rand((1,))
     output = dynet(x_train)
-    print(f'Input: {x_train.detach()}\nOutput: {output.detach()}')
+    loss = criterion(output, x_train.detach())
+    print(f'Input: {x_train.detach()}\nOutput: {output.detach()}\nLoss: {loss.detach()}')
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
     sleep(1)
--- proj4/model.py
+++ proj4/model.py
@@ -8,13 +8,28 @@ from torch import nn
 class DynamicNet(nn.Module):
     def __init__(self):
         super(DynamicNet, self).__init__()
-        self.blocks = nn.ModuleList([])
-        self.classifiers = nn.ModuleList([])
+        self.blocks = nn.ModuleList([nn.Sequential(
+                        nn.Linear(1, 10),
+                        nn.ReLU(),
+                        nn.Linear(10, 10),
+                        nn.ReLU(),
+                        nn.Linear(10, 1),
+                        nn.ReLU()
+                    )])
+        self.classifiers = nn.ModuleList([nn.Sequential(
+                        nn.Linear(1, 10),
+                        nn.ReLU(),
+                        nn.Linear(10, 10),
+                        nn.ReLU(),
+                        nn.Linear(10, 1),
+                        nn.Sigmoid()
+                    )])
 
     def forward(self, x):
         confidence = 0
         output = None
         c = 0
+        max_depth = 100
         while confidence < 1:
             try:
                 output = self.blocks[c](x)
@@ -49,4 +64,7 @@ class DynamicNet(nn.Module):
             c += 1
             print(f'Confidence: {confidence}')
             print(f'Executed Cycles: {c}')
+            if c >= max_depth:
+                print(f'Reached max depth/cycles: {max_depth}')
+                break
         return output
