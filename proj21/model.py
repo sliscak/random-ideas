5bcb05cb20b63ab958beb34a2b547a64e0b8ee6e
sliscak <64167691+sliscak@users.noreply.github.com>
2020-12-07 17:34:06 +0100

    Create model.py

---
+++ proj21/model.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+from PIL import Image
+from time import sleep
+from collections import deque, Counter
+
+class NeuralDictionary(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        # 500 keys each of size 100
+        self.keys = nn.Parameter(torch.randn(500, 100, dtype=torch.double))
+        
+        # 500 values each of size 4
+        self.values = nn.Parameter(torch.randn(500, 4, dtype=torch.double))
+
+        # see how many times a key has been chosen/called
+        # self.meta = [0 for x in range(500)]
+        self.meta = Counter()
+
+    def forward(self, query):
+        attention = torch.matmul(self.keys, query)
+        attention = torch.softmax(attention, 0)
+        out = torch.matmul(attention, self.values)
+        out = torch.sigmoid(out)
+        
+        amax = torch.argmax(attention)
+        self.meta.update(f'{amax}')
+        #print(self.meta.most_common(10))
+      
+        return out
+    
