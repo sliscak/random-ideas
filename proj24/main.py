62b37f0fafcc16d8611ca718527b5ea8f86d4cd7
sliscak <sliscak@gmail.com>
2020-12-22 21:20:54 +0100

    correction and fix bugs

--- proj24/main.py
+++ proj24/main.py
@@ -1,35 +1,6 @@
 import torch
+from models import Net, Net2
 from torch import nn
-
-
-class Net(nn.Module):
-    def __init__(self, infeatures:int, capacity:int):
-        super(Net, self).__init__()
-        self.memory = nn.ParameterList([nn.Parameter(torch.rand(infeatures)) for i in range(capacity)])
-
-    def forward(self, x):
-        distances = torch.tensor([])
-        for i in range(len(self.memory)):
-            distance = torch.cosine_similarity(x, self.memory[i], dim=0)
-            # print(distance)
-            # print(distances)
-            distances = torch.cat((distances, torch.unsqueeze(distance, dim=0)), dim=0)
-        return distances
-
-class Net2(nn.Module):
-    def __init__(self, infeatures:int, capacity:int):
-        super(Net2, self).__init__()
-        self.memory = nn.ParameterList([nn.Parameter(torch.rand(infeatures)) for i in range(capacity)])
-
-    def forward(self, x):
-        distances = torch.tensor([])
-        for i in range(len(self.memory)):
-            distance = torch.cosine_similarity(x, self.memory[i], dim=0)
-            # print(distance)
-            # print(distances)
-            distances = torch.cat((distances, torch.unsqueeze(distance, dim=0)), dim=0)
-        attention = torch.softmax(distances, dim=0)
-        return attention
 
 net = Net(3, 10)
 t = torch.tensor([1,2,3])
---
+++ proj24/models.py
@@ -0,0 +1,31 @@
+import torch
+from torch import nn
+
+class Net(nn.Module):
+    def __init__(self, infeatures:int, capacity:int):
+        super(Net, self).__init__()
+        self.memory = nn.ParameterList([nn.Parameter(torch.rand(infeatures)) for i in range(capacity)])
+
+    def forward(self, x):
+        distances = torch.tensor([])
+        for i in range(len(self.memory)):
+            distance = torch.cosine_similarity(x, self.memory[i], dim=0)
+            # print(distance)
+            # print(distances)
+            distances = torch.cat((distances, torch.unsqueeze(distance, dim=0)), dim=0)
+        return distances
+
+class Net2(nn.Module):
+    def __init__(self, infeatures:int, capacity:int):
+        super(Net2, self).__init__()
+        self.memory = nn.ParameterList([nn.Parameter(torch.rand(infeatures)) for i in range(capacity)])
+
+    def forward(self, x):
+        distances = torch.tensor([])
+        for i in range(len(self.memory)):
+            distance = torch.cosine_similarity(x, self.memory[i], dim=0)
+            # print(distance)
+            # print(distances)
+            distances = torch.cat((distances, torch.unsqueeze(distance, dim=0)), dim=0)
+        attention = torch.softmax(distances, dim=0)
+        return attention
