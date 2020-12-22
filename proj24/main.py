183212f694c06c0bbaeae5c2debf7c3ba9cbe0f2
sliscak <sliscak@gmail.com>
2020-12-22 21:41:42 +0100

    .

--- proj24/main.py
+++ proj24/main.py
@@ -1,6 +1,5 @@
 import torch
-from models import Net, Net2, Net3
-
+from models import Net, Net2, Net3, Net4
 net = Net(3, 10)
 t = torch.tensor([1,2,3])
 out = net(t.detach())
@@ -14,4 +13,9 @@ print(out)
 net3 = Net3(3, 10)
 t = torch.tensor([1,2,3])
 out = net3(t.detach())
+print(out)
+
+net4 = Net4(3, 10)
+t = torch.tensor([1,2,3])
+out = net4(t.detach())
 print(out)
--- proj24/models.py
+++ proj24/models.py
@@ -47,3 +47,20 @@ class Net3(nn.Module):
         mask[argmax] = 1
         out = distances * mask
         return out
+
+class Net4(nn.Module):
+    def __init__(self, infeatures:int, capacity:int):
+        super(Net4, self).__init__()
+        self.memory = nn.ParameterList([nn.Parameter(torch.rand(infeatures)) for i in range(capacity)])
+
+    def forward(self, x):
+        distances = torch.tensor([])
+        for i in range(len(self.memory)):
+            distance = torch.cosine_similarity(x, self.memory[i], dim=0)
+            # print(distance)
+            # print(distances)
+            distances = torch.cat((distances, torch.unsqueeze(distance, dim=0)), dim=0)
+        argmax = torch.argmax(distances, dim=0)
+        out = torch.zeros(distances.shape[0], dtype=torch.double) # could use "pytorch.sparse" here to maximize efficiency
+        out[argmax] = distances[argmax]
+        return out
