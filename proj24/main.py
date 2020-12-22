b26e8e8bfd80b067b85e7d6de41d9286a9867cd7
sliscak <sliscak@gmail.com>
2020-12-22 21:18:37 +0100

    fix bugs

--- proj24/main.py
+++ proj24/main.py
@@ -18,7 +18,7 @@ class Net(nn.Module):
 
 class Net2(nn.Module):
     def __init__(self, infeatures:int, capacity:int):
-        super(Net, self).__init__()
+        super(Net2, self).__init__()
         self.memory = nn.ParameterList([nn.Parameter(torch.rand(infeatures)) for i in range(capacity)])
 
     def forward(self, x):
@@ -34,4 +34,9 @@ class Net2(nn.Module):
 net = Net(3, 10)
 t = torch.tensor([1,2,3])
 out = net(t.detach())
+print(out)
+
+net2 = Net2(3, 10)
+t = torch.tensor([1,2,3])
+out = net2(t.detach())
 print(out)
