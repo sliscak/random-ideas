2f00165651dab0d1d7ae956fb4510d42311ec3b9
sliscak <sliscak@gmail.com>
2020-12-22 21:28:15 +0100

    .

--- proj24/main.py
+++ proj24/main.py
@@ -1,5 +1,5 @@
 import torch
-from models import Net, Net2
+from models import Net, Net2, Net3
 from torch import nn
 
 net = Net(3, 10)
@@ -8,6 +8,11 @@ out = net(t.detach())
 print(out)
 
 net2 = Net2(3, 10)
+t = torch.tensor([1,2,3])
+out = net2(t.detach())
+print(out)
+
+net3 = Net3(3, 10)
 t = torch.tensor([1,2,3])
 out = net2(t.detach())
 print(out)
--- proj24/models.py
+++ proj24/models.py
@@ -29,3 +29,21 @@ class Net2(nn.Module):
             distances = torch.cat((distances, torch.unsqueeze(distance, dim=0)), dim=0)
         attention = torch.softmax(distances, dim=0)
         return attention
+
+class Net3(nn.Module):
+    def __init__(self, infeatures:int, capacity:int):
+        super(Net3, self).__init__()
+        self.memory = nn.ParameterList([nn.Parameter(torch.rand(infeatures)) for i in range(capacity)])
+
+    def forward(self, x):
+        distances = torch.tensor([])
+        for i in range(len(self.memory)):
+            distance = torch.cosine_similarity(x, self.memory[i], dim=0)
+            # print(distance)
+            # print(distances)
+            distances = torch.cat((distances, torch.unsqueeze(distance, dim=0)), dim=0)
+        amax = torch.amax(distances, dim=0)
+        mask = torch.zeros(distances, dtype=torch.double)
+        mask[amax] = 1
+        out = distances * mask
+        return out
