57bd9b6d9f27930df5625065f4b98e17a7daa1bd
sliscak <sliscak@gmail.com>
2020-12-22 21:12:22 +0100

    .

---
+++ proj24/main.py
@@ -0,0 +1,23 @@
+import torch
+from torch import nn
+
+
+class Net(nn.Module):
+    def __init__(self, infeatures:int, capacity:int):
+        super(Net, self).__init__()
+        self.memory = nn.ParameterList([nn.Parameter(torch.rand(infeatures)) for i in range(capacity)])
+
+    def forward(self, x):
+        distances = torch.tensor([])
+        for i in range(len(self.memory)):
+            distance = torch.cosine_similarity(x, self.memory[i], dim=0)
+            # print(distance)
+            # print(distances)
+            distances = torch.cat((distances, torch.unsqueeze(distance, dim=0)), dim=0)
+        print(distances)
+        return x
+
+net = Net(3, 10)
+t = torch.tensor([1,2,3])
+out = net(t.detach())
+print(out)
